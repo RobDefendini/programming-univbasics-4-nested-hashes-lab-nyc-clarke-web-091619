02b7732dfb002ef1c9c6cf03bd59c5da288de060
Robert Defendini <robdefendini@gmail.com>
2020-05-19 00:27:03 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -83,9 +83,9 @@ def adding_matz
           :languages => ["C"]
         }
      }
-    programmer_hash[:yukihiro_matsumoto]  
-    yukihiro_matsumoto[:known_for] =  "Ruby"
-    yukihiro_matsumoto[:languages] = "LISP", "C"
+    programmer_hash[:yukihiro_matsumoto] = { 
+    :known_for  "Ruby"
+    :languages "LISP", "C"
     puts programmer_hash
 end
 
