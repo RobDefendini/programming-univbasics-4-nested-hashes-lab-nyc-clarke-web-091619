5c97c75f8b29bb1e17871e469d823fe3b15d273d
Robert Defendini <robdefendini@gmail.com>
2020-05-19 00:13:42 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -82,9 +82,9 @@ def adding_matz
           :languages => ["C"]
         }
      }
-    programmer_hash[:yukihiro_matsumoto] << [:known_for][:languages]
+    programmer_hash[:yukihiro_matsumoto] << [:known_for, "Ruby" ][:languages, "Lisp"]
     
-    programmer_hash
+    
 end
 
 def changing_alan
