3606eb53eff69666d34464d1386b95cbd3bdbf9c
Robert Defendini <robdefendini@gmail.com>
2020-05-19 00:36:31 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -128,5 +128,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+newly_added_to_hash = "Assembly"
+programmer_hash[:dennis_ritchie][:languages] => newly_added_to_hash
 end
